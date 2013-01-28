16ec27963b7af2d36b87f89c077d119990a7e969
endaroza <github@kendar.org>
2013-01-28 23:03:38 +0100

    Cleaned for 1.0.0.0

--- phpnuget/admin/pkg_index.php
+++ phpnuget/admin/pkg_index.php
@@ -45,7 +45,7 @@ for($i=0;$i<sizeof($allEntities);$i++){
                 ?>
                 <tr>
                     <td><?php echo $entity->Title;?></td><td><?php echo $entity->Version;?></td>
-                    <td><a href="<?php echo $baseUtl."/api/v2/package/".strtolower($entity->Identifier)."/".$entity->Version;?>">Download</a></td>
+                    <td><a href="<?php echo $baseUrl."/api/v2/package/?package=".strtolower($entity->Identifier).".".$entity->Version;?>">Download</a></td>
                     <td><a href="pkg_delete.php?identifier=<?php echo strtolower($entity->Identifier);?>&version=<?php echo $entity->Version;?>">Delete</a></td>
                     <td><a href="pkg_edit.php?identifier=<?php echo strtolower($entity->Identifier);?>&version=<?php echo $entity->Version;?>">Edit</a></td>
                 </tr>
--- phpnuget/nuget/old/GetUpdates()/index.php
+++
@@ -1,9 +0,0 @@
-<?php
-define('__ROOT__',dirname(dirname( dirname(__FILE__))));
-require_once(__ROOT__.'/inc/listController.php'); 
-require_once(__ROOT__.'/inc/virtualdirectory.php'); 
-$virtualDirectory = new VirtualDirectory();
-$baseUrl = $virtualDirectory->baseurl;
-$baseUrl = $virtualDirectory->upFromLevel($baseUrl,2);
-ListController::LoadAll($baseUrl);
-?>
