8bcb3415d04af784f9bc4020b69223124350f23f
endaroza <github@kendar.org>
2013-03-18 12:48:28 +0100

    Update settings.php

--- phpnuget/settings.php
+++ phpnuget/settings.php
@@ -1,7 +1,7 @@
 <?php
 define('__ROOT__', dirname(__FILE__));
 define('__UPLOAD_DIR__', __ROOT__."/sources");
-define('__PHP_NUGET_VERSION__',"0.0.0.0");
+define('__PHP_NUGET_VERSION__',"1.0.0.0");
 define('__MAXUPLOAD_BYTES__',10*1024*1024);
 define('__API_DOWNLOAD_POSITION__',__ROOT__."/api/v2/package");
 define('__PACKAGEHASH__',"SHA512"); //Or SHA256
