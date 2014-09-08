2d7a480fe829fefa98b8b8bf3ee5cd568c9bcac4
edr <fuffa@deltatre.com>
2014-09-08 09:43:37 +0200

    I hate case

--- src/assets/initialize.js
+++
@@ -1,8 +0,0 @@
-
-app.config(["pathHelperProvider", function(pathHelperProvider) {
-	var pathName = window.location.pathname.rtrim("/").ltrim("/");
-	var pathNameApi = pathName+"/api";
-	
-	pathHelperProvider.setupApiPath(pathNameApi);
-	pathHelperProvider.setupRootPath(pathName);
-}]);
