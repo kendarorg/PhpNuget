eedebd797a0300b7199a9da687de0ed1bac8c574
edr <fuffa@deltatre.com>
2014-11-07 15:15:49 +0100

    * 3.0.6.0 	* Added "tolower" function (thanks petero-dk!) 	* Added "startswith", "endswith" and "toupper" 	* Fixed issue when calling functions passing a field name as second parameter. 	* Added tests for substringof and tolower

--- tests/commons/objectsearch_4_execute.php
+++ tests/commons/objectsearch_4_execute.php
@@ -1,8 +1,10 @@
 <?php
 
 require_once(__ROOT__."/inc/commons/objectsearch.php");
+require_once(__ROOT__."/inc/commons/utils.php");
 
 Test::DoTest("ObjectSearchTest_4");
+
 
 class ObjectSearchTest_4_001
 {
@@ -85,7 +87,7 @@ class ObjectSearchTest_4
 		Assert::IsTrue($result);
 	}
 	
-	public function DoTestString_strint_eq_field()
+	public function DoTestString_string_eq_field()
 	{
 		$o = $this->BuildOb("test");
 		$r = $this->target->Parse("'test' eq Field",$o);
@@ -95,10 +97,129 @@ class ObjectSearchTest_4
 		Assert::IsTrue($result);
 	}
 	
-	public function DoTestString_strint_beq_field()
+	public function DoTestString_string_beq_field()
 	{
 		$o = $this->BuildOb("test");
 		$r = $this->target->Parse("'ciosp' neq Field",$o);
+		echo Test::PrettyDump($r);
+		$result = $this->target->Execute($o);
+		
+		Assert::IsTrue($result);
+	}
+	
+	public function DoTestString_string_substringof()
+	{
+		$o = $this->BuildOb("ciospolo");
+		
+		$r = $this->target->Parse("substringof('ciosp',Field)",$o);
+		echo Test::PrettyDump($r);
+		$result = $this->target->Execute($o);
+		
+		Assert::IsTrue($result);
+	}
+	
+	public function DoTestString_string_substringof_false()
+	{
+		$o = $this->BuildOb("ciospolo");
+		
+		$r = $this->target->Parse("substringof('ciospx',Field)",$o);
+		echo Test::PrettyDump($r);
+		$result = $this->target->Execute($o);
+		
+		Assert::IsFalse($result);
+	}
+	
+	public function DoTestString_string_substringof_inverted()
+	{
+		$o = $this->BuildOb("ciosp");
+		
+		$r = $this->target->Parse("substringof(Field,'ciospolo')",$o);
+		echo Test::PrettyDump($r);
+		$result = $this->target->Execute($o);
+		
+		Assert::IsTrue($result);
+	}
+	
+	public function DoTestString_string_substringof_inverted_false()
+	{
+		$o = $this->BuildOb("ciospx");
+		
+		$r = $this->target->Parse("substringof(Field,'ciospolo')",$o);
+		echo Test::PrettyDump($r);
+		$result = $this->target->Execute($o);
+		
+		Assert::IsFalse($result);
+	}
+	
+	public function DoTestString_string_tolower_neq()
+	{
+		$o = $this->BuildOb("CIOSP");
+		$r = $this->target->Parse("tolower(Field) neq 'CIOSP'",$o);
+		
+		$result = $this->target->Execute($o);
+		
+		Assert::IsTrue($result);
+	}
+	
+	public function DoTestString_string_tolower_neq_diff_case()
+	{
+		$o = $this->BuildOb("CIOSP");
+		$r = $this->target->Parse("tolower(Field) neq 'ciosp'",$o);
+		
+		$result = $this->target->Execute($o);
+		
+		Assert::IsFalse($result);
+	}
+	
+	public function DoTestString_string_tolower_eq()
+	{
+		$o = $this->BuildOb("CIOSP");
+		
+		$r = $this->target->Parse("tolower(Field) eq 'ciosp'",$o);
+		echo Test::PrettyDump($r);
+		$result = $this->target->Execute($o);
+		
+		Assert::IsTrue($result);
+	}	
+	
+	public function DoTestMixAndMatch()
+	{
+		$o = $this->BuildOb("CIOSP");
+		
+		$r = $this->target->Parse("substringof(tolower(Field),'ciospolo')",$o);
+		echo Test::PrettyDump($r);
+		$result = $this->target->Execute($o);
+		
+		Assert::IsTrue($result);
+	}
+	
+	public function DoTestMixAndMatchFalse()
+	{
+		$o = $this->BuildOb("CIOSPx");
+		
+		$r = $this->target->Parse("substringof(tolower(Field),'ciospolo')",$o);
+		echo Test::PrettyDump($r);
+		$result = $this->target->Execute($o);
+		
+		Assert::IsFalse($result);
+	}
+	
+	//substringof('class',tolower(Id))
+	public function DoTestMixAndMatch_nest()
+	{
+		$o = $this->BuildOb("CIOSP");
+		
+		$r = $this->target->Parse("(Field eq 'CIOSP') and (substringof(tolower(Field),'ciospolo'))",$o);
+		echo Test::PrettyDump($r);
+		$result = $this->target->Execute($o);
+		
+		Assert::IsTrue($result);
+	}
+	public function DoTestMixAndMatch_nest_invert()
+	{
+		$o = $this->BuildOb("CIOSPOLO");
+		
+		$r = $this->target->Parse("(Field neq 'ciospolo') and (substringof('ciosp',tolower(Field)))",$o);
 		echo Test::PrettyDump($r);
 		$result = $this->target->Execute($o);
 		
