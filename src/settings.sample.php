f428467f192914a4235323b70caed6e4da6f9034
bkraul <bkraul@yahoo.com>
2019-01-26 11:08:53 -0600

    Add a sample settings file and ignore the user settings file. Ignore all files in src/data

---
+++ src/settings.sample.php
@@ -0,0 +1,34 @@
+<?php
+require_once(__DIR__."/root.php");
+
+define('__MAXUPLOAD_BYTES__',10*1024*1024*1024);
+define('__PACKAGEHASH__',"SHA512"); //Or SHA256
+define('__UPLOAD_DIR__', "C:\\Kendar\\Development\\PhpNuget_Evo\\PhpNuget\\src\\data\\packages");
+define('__DATABASE_DIR__', "C:\\Kendar\\Development\\PhpNuget_Evo\\PhpNuget\\src\\data\\db");
+define('__SITE_ROOT__', "/edsa-nuget/");
+define('__RESULTS_PER_PAGE__', 20);
+define('__ADMINID__',"admin");
+define('__ADMINPASSWORD__',"password");
+define('__ADMINMAIL__',"nuget@localhost");
+define('__PWDREGEX__',"/^.{8,40}$/");
+define('__PWDDESC__',"Min len 8, max len 40");
+
+//If false "Register" is disabled. Users would be only allowed to be registered by
+//the admin
+define('__ALLOWUSERADD__',false);
+
+define('__ALLOWPACKAGESDELETE__',true);
+define('__ALLOWPACKAGEUPDATE__', true);
+
+@define('__MYSQL_SERVER__', "127.0.0.1");
+@define('__MYSQL_USER__',"root");
+@define('__MYSQL_PASSWORD__',"");
+@define('__MYSQL_DB__',"phpnuget");
+@define('__DB_TYPE__',DBMYSQL);
+
+//If true users are allowed to add a package only if the firstly added it
+//or if theyr user id is inside the "owners" field of the package
+define('__LIMITUSERSPACKAGES__',true);
+
+require_once(__ROOT__."/inc/internalsettings.php");
+?>
