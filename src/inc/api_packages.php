a5619a5425da28cf15071b317edf0775bbb882f5
Michael Welle <michael.welle@h-brs.de>
2019-02-05 12:13:37 +0100

    Honour a web proxy setting when fetching external nuget packages
    Use a configured http proxy when fetching external nuget packages.
    If the configuration option is not set, try to retrieve the value
    of the http_proxy environment variable and use that as the proxy.
    If both fails, don't use a proxy.

--- src/inc/api_packages.php
+++ src/inc/api_packages.php
@@ -25,6 +25,15 @@ class SingleResult
 }
 
 function getSslPage($url) {
+
+    if ( defined('__HTTPPROXY__') && (__HTTPPROXY__ !== '')) {
+      $proxy = __HTTPPROXY__;
+    } elseif (getenv('http_proxy')) {
+      $proxy = getenv('http_proxy');
+    } else {
+      $proxy = null;
+    }
+    
     $ch = curl_init();
     curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, FALSE);
     curl_setopt($ch, CURLOPT_HEADER, false);
@@ -32,6 +41,7 @@ function getSslPage($url) {
     curl_setopt($ch, CURLOPT_URL, $url);
     curl_setopt($ch, CURLOPT_REFERER, $url);
     curl_setopt($ch, CURLOPT_RETURNTRANSFER, TRUE);
+    curl_setopt($ch, CURLOPT_PROXY, $proxy);
     $result = curl_exec($ch);
     curl_close($ch);
     return $result;
