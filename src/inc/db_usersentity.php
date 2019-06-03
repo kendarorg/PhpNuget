ad07745b3cbdeffdd38e07c2e20616c65480eec1
kendarorg <github@kendar.org>
2019-06-03 16:46:08 +0200

    Alternate gravatar allow

--- src/inc/db_usersentity.php
+++ src/inc/db_usersentity.php
@@ -2,18 +2,35 @@
 require_once(dirname(__FILE__)."/../root.php");
 require_once(__ROOT__."/settings.php");
 
-class UserEntity
-{
-	var $Id;
-    var $Admin="false";
-    var $UserId;
-    var $Name;
-    var $Company;
-    var $Md5Password;
-    var $Packages;
-    var $Enabled;
-    var $Email;
-	var $GravatarUrl;
-    var $Token;
+if(defined('__ALLOWGRAVATAR__') && __ALLOWGRAVATAR__) {
+    class UserEntity
+    {
+        var $Id;
+        var $Admin = "false";
+        var $UserId;
+        var $Name;
+        var $Company;
+        var $Md5Password;
+        var $Packages;
+        var $Enabled;
+        var $Email;
+        var $GravatarUrl;
+        var $Token;
+    }
+}else{
+    class UserEntity
+    {
+        var $Id;
+        var $Admin = "false";
+        var $UserId;
+        var $Name;
+        var $Company;
+        var $Md5Password;
+        var $Packages;
+        var $Enabled;
+        var $Email;
+        var $Token;
+    }
 }
 ?>
+
