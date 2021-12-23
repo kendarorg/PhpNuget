f7ab8f4df8371276461b43b891050d9b346c1df2
Kendar <unknown@kendar.org>
2021-12-23 09:51:52 +0100

    Converter fetch_assoc to object

--- src/lib/nuget/fields/mysql/NugetPackageConverter.php
+++ src/lib/nuget/fields/mysql/NugetPackageConverter.php
@@ -2,29 +2,42 @@
 
 namespace lib\nuget\fields\mysql;
 
+use lib\nuget\models\NugetDependency;
 use lib\nuget\models\NugetPackage;
+use lib\utils\JsonMapper;
 use ReflectionClass;
 use ReflectionProperty;
 
 class NugetPackageConverter
 {
+    public function __construct()
+    {
+        $this->objects = ["Dependencies" => new NugetDependency()];
+        $this->arrays =  ["Author", "Owners", "References"];
+    }
+
     /**
      * @var string[]
      */
-    private array $objects = ["Dependencies" => "func"];
+    private array $objects = [];
     /**
      * @var string[]
      */
-    private array $arrays = ["Author", "Owners", "References"];
+    private array $arrays =[];
 
     public function fromAssoc($data)
     {
         $result = new NugetPackage();
+        $mapper = new JsonMapper();
+
         foreach ($data as $key => $value) {
             if (in_array($key, $this->arrays)) {
-
-            } else if (in_array($key, $this->objects)) {
-
+                $result->$key = json_decode($value);
+            } else if (isset( $this->objects[$key])) {
+                $result->$key = $mapper->map(
+                    json_decode($value),
+                    $this->objects[$key]
+                );
             } else {
                 $result->$key = $value;
             }
@@ -41,10 +54,8 @@ class NugetPackageConverter
         foreach ($vars as $privateVar) {
             $key = $privateVar->getName();
             $value = $privateVar->getValue($data);
-            if (in_array($key, $this->arrays)) {
-
-            } else if (in_array($key, $this->objects)) {
-
+            if (in_array($key, $this->arrays) || isset( $this->objects[$key])) {
+                $result[$key] = json_encode($value);
             } else {
                 $result[$key] = $value;
             }
